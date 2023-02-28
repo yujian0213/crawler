c86f12f26bf59f1b229f20bf82b927b4d6594261
yujian <563568371@qq.com>
2023-02-28 17:24:12 +0800

    borwser

---
+++ collect/collect.go
@@ -0,0 +1,67 @@
+package collect
+
+import (
+	"bufio"
+	"fmt"
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
+	"io"
+	"net/http"
+)
+
+type Fetcher interface {
+	Get(url string) ([]byte, error)
+}
+type BaseFetch struct {
+}
+
+func (bf BaseFetch) Get(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("Error status code:%d", resp.StatusCode)
+	}
+	bodyReader := bufio.NewReader(resp.Body)
+	e := DeterminEncoding(bodyReader)
+	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
+	return io.ReadAll(utf8Reader)
+}
+func DeterminEncoding(r *bufio.Reader) encoding.Encoding {
+
+	bytes, err := r.Peek(1024)
+
+	if err != nil {
+		fmt.Printf("fetch error:%v\n", err)
+		return unicode.UTF8
+	}
+
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
+}
+
+type BrowserFetch struct {
+}
+
+// 模拟浏览器访问
+func (BrowserFetch) Get(url string) ([]byte, error) {
+	client := http.Client{}
+	req, err := http.NewRequest("get", url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("get url failed:%v", err)
+	}
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36")
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	bodyReader := bufio.NewReader(resp.Body)
+	e := DeterminEncoding(bodyReader)
+	uft8Reader := transform.NewReader(bodyReader, e.NewDecoder())
+	return io.ReadAll(uft8Reader)
+}
--- main.go
+++ main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"crawler/collect"
+	"fmt"
+)
+
+func main() {
+	url := "https://book.douban.com/subject/1007305/"
+	var f collect.Fetcher = collect.BrowserFetch{}
+	body, err := f.Get(url)
+	if err != nil {
+		fmt.Println("read content failed:%v", err)
+		return
+	}
+	fmt.Println(string(body))
+}
