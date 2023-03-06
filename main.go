72326726c54ad301a06ff25502891a3efa7202cd
yujian <563568371@qq.com>
2023-03-06 13:55:26 +0800

    chromedp

--- main.go
+++ main.go
@@ -1,17 +1,34 @@
 package main
 
 import (
-	"crawler/collect"
-	"fmt"
+	"github.com/chromedp/chromedp"
+	"golang.org/x/net/context"
+	"log"
+	"time"
 )
 
 func main() {
-	url := "https://book.douban.com/subject/1007305/"
-	var f collect.Fetcher = collect.BrowserFetch{}
-	body, err := f.Get(url)
+	// 1、创建谷歌浏览器实例
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+	// 2、设置context超时时间
+	ctx, cancel = context.WithTimeout(ctx, 15*time.Second)
+	defer cancel()
+	// 3、爬取页面，等待某一个元素出现,接着模拟鼠标点击，最后获取数据
+	var example string
+	err := chromedp.Run(ctx,
+		chromedp.Navigate(`https://pkg.go.dev/time`),
+
+		chromedp.WaitVisible(`body > footer`),
+
+		chromedp.Click(`#example-After`, chromedp.NodeVisible),
+
+		chromedp.Value(`#example-After textarea`, &example),
+	)
 	if err != nil {
-		fmt.Println("read content failed:%v", err)
-		return
+
+		log.Fatal(err)
+
 	}
-	fmt.Println(string(body))
+	log.Printf("Go's time.After example:\\n%s", example)
 }
